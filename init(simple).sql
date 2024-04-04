aff9bc1a5d0a968d950d8531b04fc9bb408dc201
Arkant04 <delacorte19@gmail.com>
2024-04-04 12:53:19 +0200

    MOD Se termina por hoy la clase de SQL

---
+++ init(simple).sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS mensajes;
+
+CREATE TABLE 'usuarios'(
+    
+);
+
+
+
+INSERT INTO usuarios VALUES(1, 'admin', 33);
+INSERT INTO usuarios VALUES(2, 'user0', 12);
+INSERT INTO usuarios VALUES(3, 'user1', 41);
+INSERT INTO usuarios VALUES(4, 'user2', 31);
+INSERT INTO usuarios VALUES(5, 'user3', 21);
+
+--SELECT * FROM usuarios;
+--SELECT * FROM usuarios WHERE edad > 25;
--- init.sql
+++ init.sql
@@ -1,49 +1,3 @@
-**Esto es lo que hemos dado el 5 de abril sobre "SQL"
-
--CREATE table "nombre de la tabla"
--SELECT * from "nombre de la tabla"
--INSERT into "nombre tabla"("pones lo que quieres seleccionar de la tabla") values ("lo que quieres añadir")
--.schema, .table, .open, .clone 
--DROP table "nombre de la tabla"
--STRIC Sirve para que los datos que le pasemos sean del tipo que le hemos indicado
--COUNT cuanta los elementos que tienes 
--DISTINC devuelve los valores distintos de una tabla
-```SQL
- select distinct (pais) from usuarios;
-```
-Te muestra esto:
-    España
-    Rusia
-    Suecia
-    Colombia
-    Italia
-
-`sqlite3 ejemplo.db` para arrancar una base de datos, en cuanto mando un comando create, el archivo se crea automaticamente.
-`abrir sqlite3`, creamos una tabla y usamos `clone` para crearla, luego la podemos abrir con `.open`.
-
-
-
-```SQL
-DROP TABLE IF EXISTS usuarios;
-DROP TABLE IF EXISTS mensajes;
-
-CREATE TABLE 'usuarios'('id', 'nombre', 'edad');
-
-CREATE TABLE 'mensajes'('id', 'mensajes', 'autor');
-
-INSERT INTO usuarios VALUES(1, 'admin', 33);
-INSERT INTO usuarios VALUES(2, 'user0', 12);
-INSERT INTO usuarios VALUES(3, 'user1', 41);
-INSERT INTO usuarios VALUES(4, 'user2', 31);
-INSERT INTO usuarios VALUES(5, 'user3', 21);
-
---SELECT * FROM usuarios;
---SELECT * FROM usuarios WHERE edad > 25;
-```
-                                  arriba
-Para crear una base datos por codigo| 
-                                  abajo
-```SQL
 DROP TABLE IF EXISTS mensajes;
 DROP TABLE IF EXISTS usuarios;
 PRAGMA foreign_keys = on;
@@ -82,4 +36,5 @@ INSERT INTO mensajes (mensaje, autor) VALUES ('Pizza', 5);
 
 --SELECT * FROM usuarios;
 --SELECT * FROM usuarios WHERE edad > 25;
-```
+
+
